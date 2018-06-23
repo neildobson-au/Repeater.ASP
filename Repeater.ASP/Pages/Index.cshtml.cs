01b9da903a5ac7ea46ad0cd1d8b32801d7a6553c
Neil Dobson <neil.dobson71@gmail.com>
2018-06-23 14:18:34 +1000

    Use EditorTemplates instead of Partial to get correct ModelState keys. Styling, Clean-up

--- Repeater.ASP/Pages/Index.cshtml.cs
+++ Repeater.ASP/Pages/Index.cshtml.cs
@@ -16,8 +16,56 @@ namespace Repeater.ASP.Pages
             {
                 States = new List<State>
                 {
-                    new State {Name = "ACT", IsTerritory = true},
-                    new State {Name = "NSW"},
+                    new State
+                    {
+                        Name = "ACT", 
+                        IsTerritory = true
+                    },
+                    new State
+                    {
+                        Name = "NSW",
+                        Locations = new List<Location>
+                        {
+                            new Location { PostCode = 2000, Suburb = "Sydney" },
+                            new Location
+                            {
+                                PostCode = 2065, 
+                                Suburb = "Wollstonecraft",
+                                Streets = new List<Street>
+                                {
+                                    new Street
+                                    {
+                                        Name = "Nicholson St",
+                                        Homes = new List<Home>
+                                        {
+                                            new Home 
+                                            { 
+                                                Number = 1, 
+                                                Rooms = new List<Room>
+                                                {
+                                                    new Room { Name = "Lounge", Length = 650, Width = 450 },
+                                                    new Room { Name = "Kitchen", Length = 280, Width = 330 },
+                                                    new Room { Name = "Bedroom", Length = 400, Width = 250 }
+                                                }
+                                            },
+                                            new Home 
+                                            { 
+                                                Number = 2, 
+                                                Rooms = new List<Room>
+                                                {
+                                                    new Room { Name = "Lounge", Length = 550, Width = 630 },
+                                                    new Room { Name = "Kitchen", Length = 300, Width = 340 },
+                                                    new Room { Name = "Bedroom 1", Length = 420, Width = 275 },
+                                                    new Room { Name = "Bedroom 2", Length = 420, Width = 245 }
+                                                }
+                                            }
+                                        }
+                                    },
+                                    new Street { Name = "Belmont St" }
+                                }
+                            }
+                        }
+                    },
                     new State {Name = "NT", IsTerritory = true},
                     new State {Name = "QLD"},
                     new State {Name = "SA"},
