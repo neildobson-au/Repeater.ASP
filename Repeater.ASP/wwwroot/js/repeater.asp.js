4a12610b281117e1c3308410de2a1cb849a62b27
Neil Dobson <ndobson@barnardos.org.au>
2018-06-27 22:10:15 +1000

    Scaffold javascript

---
+++ Repeater.ASP/wwwroot/js/repeater.asp.js
@@ -0,0 +1,12 @@
+﻿var repeater = function(selector, oldPrefix, newPrefix) {
+    $(selector).find("*").each(function() {
+        $.each(this.attributes,
+            function(i, attrib) {
+                if (!attrib.value.startsWith("EmptyLocation")) return;
+
+                console.debug(attrib.value);
+            });
+    });
+};
+
+repeater("#EmptyLocation_repeaterRow");
